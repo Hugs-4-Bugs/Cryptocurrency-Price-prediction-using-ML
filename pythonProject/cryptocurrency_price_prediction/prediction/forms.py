b9b993781314fa50438e5fc9e879d75899e68817
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-04-25 01:37:48 +0530

    Delete pythonProject directory

--- pythonProject/cryptocurrency_price_prediction/prediction/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .model.data_collector import *
-
-
-class PredictionForm(forms.Form):
-    date = forms.DateField(label='Date', widget=forms.DateInput(attrs={'type': 'date'}))
-    coin = forms.ChoiceField(choices=get_coin_list())
-
